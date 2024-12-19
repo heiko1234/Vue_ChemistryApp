8d41d1d1ca8e4ccc7f1b2268974cd19656d69a90
Heiko Kulinna <Heikokulinna@gmx.de>
2024-12-19 05:22:30 +0100

    [HK] content info

---
+++ vue_chemistry/src/components/HomeViewContent/home_content_info.vue
@@ -0,0 +1,217 @@
+<template>
+    <div class="HomeProductInfo">
+        <div class="HomeProductInfoContent">
+            <h1 class="HomeTitleAbout"> About </h1>
+            <div class="HomeProductInfoContainer">
+                <div class="HomeInfoText">
+                    <p>
+                        {{homeinfotext1}}
+                    </p>
+                    <p>
+                        {{homeinfotext2}}
+                    </p>
+                    <p>
+                        {{homeinfotext3}}
+                    </p>
+                </div>
+                <!-- div for buttons and graphics -->
+                <div class="rightInfoContent">
+                    <!-- Buttons -->
+                    <div class="homeInfoButton">
+                        <button class="about_button"
+                        :class="{active: selectedButton === 'App1'}"
+                        @click="changeGraphic('graphic1')"
+                        >
+                        App1
+                        </button>
+                        <button class="about_button"
+                        :class="{active: selectedButton === 'App2'}"
+                        @click="changeGraphic('graphic2')"
+                        >
+                        App2
+                        </button>
+                        <button class="about_button"
+                        :class="{active: selectedButton === 'App3'}"
+                        @click="changeGraphic('graphic3')"
+                        >
+                        App3
+                        </button>
+
+                    </div>
+                    <!-- Graphics -->
+                    <div class="HomeInfoGraphic">
+                        <img :src="selectedGraphic" alt="Graphic" class="homecontentgraphic" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Apollo from '@/assets/theme/Apollo.jpeg'
+import Apollo2 from '@/assets/theme/Apollo2.jpeg'
+import Apollo3 from '@/assets/theme/Apollo3.jpeg'
+
+export default {
+    name: "HomeAboutInfo",
+    props: {
+        homeinfotext1: {
+            type: String,
+            required: true
+        },
+        homeinfotext2: {
+            type: String,
+            required: true
+        },
+        homeinfotext3: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            selectedButton: 'App1',
+            selectedGraphic: Apollo
+        }
+    },
+    methods: {
+        changeGraphic(graphic) {
+            if (graphic === 'graphic1') {
+                this.selectedGraphic = Apollo
+                this.selectedButton = 'App1'
+            } else if (graphic === 'graphic2') {
+                this.selectedGraphic = Apollo2
+                this.selectedButton = 'App2'
+            } else if (graphic === 'graphic3') {
+                this.selectedGraphic = Apollo3
+                this.selectedButton = 'App3'
+            }
+        },
+        setActiveButton(button) {
+            this.selectedButton = button
+        }
+    },
+    mounted() {
+        this.changeGraphic('graphic1')
+    }
+
+}
+</script>
+
+<style>
+
+.HomeProductInfo {
+    display: block;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    background-color: white;
+}
+
+.HomeProductInfoContent {
+    display: block;
+    justify-content: center;
+    align-items: center;
+    background: transparent;
+    padding: 1rem;
+}
+
+.HomeProductInfoContainer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: transparent;
+    padding: 1rem;
+}
+
+.HomeTitleAbout {
+    font-size: 2rem;
+    color: black;
+    text-align: center;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    padding: 0;
+}
+
+.HomeInfoText {
+    flex: 1;
+    padding: 1rem;
+    font-size: 1rem;
+    color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.rightInfoContent {
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background: transparent;
+    padding: 1rem;
+}
+
+.homeInfoButton {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: transparent;
+    padding: 1rem;
+}
+
+.about_button {
+    background-color: transparent;
+    color: var(--secondary-color);
+    border: 1px solid var(--secondary-color);
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 1rem;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 12px;
+    padding: 10px 24px;
+}
+
+.about_button:hover:not(.active) {
+    background-color: var(--secondary-color);
+    color: white;
+    font-size: 1rem;
+    font-weight: bold;
+    box-shadow: 0 0 15px var(--secondary-color);
+}
+
+.about_button.active {
+    background-color: var(--secondary-color);
+    color: white;
+    font-size: 1rem;
+    font-weight: bold;
+    box-shadow: 0 0 15px var(--secondary-color);
+}
+
+.HomeInfoGraphic {
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background: transparent;
+    padding: 1rem;
+}
+
+.homecontentgraphic {
+    width: auto;
+    max-height: 500px;
+    border-radius: 12px;
+}
+</style>
+
+
+
+
+
+
+
--- vue_chemistry/src/views/HomeView.vue
+++ vue_chemistry/src/views/HomeView.vue
@@ -4,7 +4,11 @@
 <template>
   <div>
     <promotionheader :promotion_title_text="promotion_title_text" :promotion_subtitle_texta="promotion_subtitle_texta" :promotion_subtitle_textb="promotion_subtitle_textb" />
-    <homeviewheader @scroll-to-section="handleScrollToSection" />
+    <homeviewheader @scroll-to-section="handleScrollToSection" @open-contact-modal="toggleModal"/>
+    <ContactModal :isContactModalVisible="isModalVisible" @closeContactModal="toggleModal">
+      <h1>Contacts</h1>
+      <p>My contacts</p>
+    </ContactModal>
     <div class="scroll-div">
         <section id="Product">
             <!-- <h1> Product</h1> -->
@@ -16,7 +20,12 @@
         />
         </section>
         <section id="About">
-            <h1> About</h1>
+            <!-- <h1> About</h1> -->
+            <HomeAboutInfo
+            :homeinfotext1="about_text"
+            :homeinfotext2="about_text2"
+            :homeinfotext3="about_text3"
+            />
         </section>
 
         <section id="Tutorials">
@@ -31,16 +40,23 @@
 import promotionheader from '@/components/HomeViewContent/promotionheader.vue'
 import homeviewheader from '@/components/HomeViewContent/homeviewheader.vue'
 import productinfo from '@/components/HomeViewContent/home_product_info.vue'
+import ContactModal from '@/components/HomeViewContent/contactmodal.vue'
+import HomeAboutInfo from '@/components/HomeViewContent/home_content_info.vue'
 
 export default {
     name:"HomeView",
     components: {
         promotionheader,
         homeviewheader,
-        productinfo
+        ContactModal,
+        productinfo,
+        HomeAboutInfo
     },
     data() {
         return {
+            //Modal
+            isModalVisible: false,
+            // Promotionheader
             promotion_title_text: "Artemis & Apollo",
             promotion_subtitle_texta: "Build on the Past",
             promotion_subtitle_textb: " - Look to the Future",
@@ -49,6 +65,10 @@ export default {
             info_product_subtitle: "The God of the future",
             info_product_shortdescription: "Apollo is the god of the future. He is the god of prophecy, music, healing, light, and truth. He is the son of Zeus and Leto, and the twin brother of Artemis. Apollo is the ideal of the kouros, which means he has a beardless, athletic youth. He is also the god of the sun, and is often depicted with a bow and arrow.",
             info_text: "Apollo is the god of the future. He is the god of prophecy, music, healing, light, and truth. He is the son of Zeus and Leto, and the twin brother of Artemis. Apollo is the ideal of the kouros, which means he has a beardless, athletic youth. He is also the god of the sun, and is often depicted with a bow and arrow.",
+            // HomeAboutInfo
+            about_text: "Apollo is the god of the future. He is the god of prophecy, music, healing, light, and truth. He is the son of Zeus and Leto, and the twin brother of Artemis. Apollo is the ideal of the kouros, which means he has a beardless, athletic youth. He is also the god of the sun, and is often depicted with a bow and arrow.",
+            about_text2: "Apollo is the god of the future. He is the god of prophecy, music, healing, light, and truth. He is the son of Zeus and Leto, and the twin brother of Artemis. Apollo is the ideal of the kouros, which means he has a beardless, athletic youth. He is also the god of the sun, and is often depicted with a bow and arrow.",
+            about_text3: "Apollo is the god of the future. He is the god of prophecy, music, healing, light, and truth. He is the son of Zeus and Leto, and the twin brother of Artemis. Apollo is the ideal of the kouros, which means he has a beardless, athletic youth. He is also the god of the sun, and is often depicted with a bow and arrow.",
         }
     },
     methods: {
@@ -57,13 +77,22 @@ export default {
             if (element) {
                 element.scrollIntoView({ behavior: 'smooth' });
             }
+        },
+        toggleModal() {
+            this.isModalVisible = !this.isModalVisible
         }
     }
 
 }
 </script>
 
-<style>
+<style scoped>
+
+.scroll-div {
+    overflow-y: auto;
+    height: calc(100vh-200px);
+    scroll-behavior: smooth;
+}
 
 </style>
 
