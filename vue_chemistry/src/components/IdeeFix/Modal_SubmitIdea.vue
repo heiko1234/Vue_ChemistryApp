313d1fe01cb7a157e4ac0fc60b2cc25f08552d0d
Heiko Kulinna <Heikokulinna@gmx.de>
2024-12-24 14:14:52 +0100

    [HK] close button for idea modal

--- vue_chemistry/src/components/IdeeFix/Modal_SubmitIdea.vue
+++ vue_chemistry/src/components/IdeeFix/Modal_SubmitIdea.vue
@@ -4,6 +4,28 @@
       <div class="ideamodal">
         <div>
             <h1> Submit Ideas</h1>
+            <hr />
+            <div class="ideaflex">
+                <h2>Name</h2>
+                <input class="input_text" type="text" />
+            </div>
+            <div class="ideaflex">
+                <h2>Email</h2>
+                <input class="input_text" type="email" />
+            </div>
+            <div class="ideaflex">
+                <h2>Idea Title</h2>
+                <input class="input_text" type="text" />
+            </div>
+            <div class="ideaflex">
+                <h2>Idea Description</h2>
+                <textarea class="input_text_larger"></textarea>
+            </div>
+            <div class="ideaflex">
+                <button class="submit_button">Submit</button>
+                <button class="cancel_button" @click.self="closeIdeaModal">Cancel</button>
+                <button class="close_button" @click.self="closeIdeaModal">Close</button>
+            </div>
         </div>
       </div>
     </div>
@@ -30,25 +52,89 @@ export default {
 
 <style scoped>
 .backdrop {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 10;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 10;
 }
 
 .ideamodal {
-  width: 90%;
-  max-width: 40rem;
-  background: white;
-  border-radius: 6px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  z-index: 10;
-  overflow: hidden;
+    width: 90%;
+    max-width: 40rem;
+    background: white;
+    border-radius: 6px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    z-index: 10;
+    overflow: hidden;
 }
+
+.ideaflex{
+    display: flex;
+    justify-content: space-between;
+    padding: 0 1rem;
+    margin: 1rem 0;
+}
+
+.input_text{
+    width: 80%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    border-radius: 24px;
+}
+
+.input_text_larger{
+    width: 80%;
+    height: 100px;
+    overflow-y: auto;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    border-radius: 24px;
+}
+
+.submit_button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 20px 10px;
+    border: none;
+    cursor: pointer;
+    width: 80%;
+    border-radius: 24px;
+}
+
+.cancel_button {
+    background-color: #fa9702;
+    color: white;
+    padding: 14px 20px;
+    margin: 20px 10px;
+    border: none;
+    cursor: pointer;
+    width: 80%;
+    border-radius: 24px;
+}
+
+.close_button {
+    background-color: #f44336;
+    color: white;
+    padding: 14px 20px;
+    margin: 20px 10px;
+    border: none;
+    cursor: pointer;
+    width: 80%;
+    border-radius: 24px;
+}
+
+
 </style>
